ea5751c8b93e2488970b4a93378da45657feee0d
Marco Zepeda <123316474+marcozee03@users.noreply.github.com>
2026-04-02 01:13:00 -0400

    init

---
+++ player_cotnroller.gd
@@ -0,0 +1,38 @@
+extends CharacterBody2D
+
+@export
+var speed : float = 300.0
+@export
+var acceleration : float = 300.0
+@export
+var deceleration : float = 300.0
+@export
+var jump_velocity : float = -400.0
+@export
+var sprite : Sprite2D
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		if velocity.y <= 0:
+			velocity += get_gravity() * delta
+		else:
+			velocity += get_gravity() * 2 * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = jump_velocity
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("ui_left", "ui_right")
+	if direction:
+		if direction < 0:
+			sprite.flip_h = false
+		else:
+			sprite.flip_h = true
+		velocity.x = move_toward(velocity.x, direction * speed, delta * acceleration)
+	else:
+		velocity.x = move_toward(velocity.x, 0, deceleration * delta)
+
+	move_and_slide()
